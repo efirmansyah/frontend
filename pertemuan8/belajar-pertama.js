d287f4e5179c6646a555283df59382c902cf4779
efirmansyah <endarfirmansyah@gmail.com>
2021-02-10 20:24:37 +0700

    Pertemuan FrontEnd Web 4-7

---
+++ pertemuan8/belajar-pertama.js
@@ -0,0 +1,7 @@
+let any= document.getElementById("tombol")
+console.log(any);
+
+document.getElementById("welcome").innerHTML = "Asep";
+
+let namaDepan = prompt("What's your name");
+console.log(namaDepan);
---
+++ pertemuan8/testpemahaman.js
@@ -0,0 +1,3 @@
+let buah = prompt("Enter the first choice:");
+document.getElementById("buah").innerHTML = buah;
+console.log(buah);
